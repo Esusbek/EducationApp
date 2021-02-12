1838a8bd7441605b992d38b54e9eaffefe94debf
Esusbek <dimmonn13@gmail.com>
2021-02-12 12:18:41 +0200

    Initializer attempt

--- EducationApp.DataAccessLayer/Initialization/DataBaseInitializer.cs
+++ EducationApp.DataAccessLayer/Initialization/DataBaseInitializer.cs
@@ -1,57 +1,76 @@
 ﻿using EducationApp.DataAccessLayer.Entities;
+using EducationApp.Shared.Enums;
 using Microsoft.AspNetCore.Identity;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace EducationApp.DataAccessLayer.Initialization
 {
     public class DataBaseInitializer
     {
-        public static void DatabaseInitialization()
+        private AppContext.ApplicationContext _appContext;
+        private readonly UserManager<UserEntity> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public DataBaseInitializer(UserManager<UserEntity> userManager, RoleManager<IdentityRole> roleManager,
+            AppContext.ApplicationContext appContext)
         {
-            var hasher = new PasswordHasher<UserEntity>();
-            //modelBuilder.Entity<PrintingEditionEntity>().HasData(new PrintingEditionEntity 
-            //{ 
-            //    Id = 1,
-            //    IsRemoved = false,
-            //    Currency = Entities.Enums.Enums.Currency.UAH, 
-            //    Price = 200.05M, 
-            //    Description = "Initial test PE", 
-            //    Title = "Test PE", 
-            //    Type = Entities.Enums.Enums.PrintingEdition.Type.Book, 
-            //    Status = Entities.Enums.Enums.PrintingEdition.Status.InStock
-            //});
-            //modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
-            //{
-            //    Id = "1",
-            //    Name = "Admin",
-            //    NormalizedName = "Admin".ToLower()
-            //});
-            //modelBuilder.Entity<AuthorEntity>().HasData(new AuthorEntity
-            //{
-            //    Id = 1,
-            //    Name = "Vasily",
-            //    IsRemoved = false
-            //});
-            //modelBuilder.Entity<UserEntity>().HasData(new UserEntity
-            //{
-            //    Id = "1",
-            //    FirstName = "Vitaly",
-            //    LastName = "Pupkin",
-            //    Email = "test@test.com",
-            //    UserName = "Admin",
-            //    NormalizedUserName = "Admin".ToLower(),
-            //    PasswordHash = hasher.HashPassword(null, "password"),
-            //    NormalizedEmail = "test@test.com".ToLower(),
-            //    EmailConfirmed = true,
-            //    SecurityStamp = string.Empty,
-            //    IsRemoved = false
-            //});
-            //modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
-            //{
-            //    RoleId = "1",
-            //    UserId = "1"
-            //});
+            _appContext = appContext;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
 
+        public async Task Seed()
+        {
+            var existingPE = _appContext.PrintingEditions.FirstOrDefault(pe => pe.Title == "Test PE");
+            if(_appContext.Authors.Any())
+            {
+                return;
+            }
+            _appContext.Set<PrintingEditionEntity>().Add(new PrintingEditionEntity
+                {
+                    Id = 1,
+                    IsRemoved = false,
+                    Currency = Enums.Currency.UAH,
+                    Price = 200.05M,
+                    Description = "Initial test PE",
+                    Title = "Test PE",
+                    Type = Enums.PrintingEdition.Type.Book,
+                    Status = Enums.PrintingEdition.Status.InStock
+                });
+            _appContext.Set<AuthorEntity>().Add(new AuthorEntity
+            {
+                Id = 1,
+                Name = "Vasily",
+                IsRemoved = false,
+                PrintingEditions = new List<PrintingEditionEntity>()
+            });
+            _appContext.SaveChanges();
+            await _roleManager.CreateAsync(new IdentityRole
+                {
+                    Name = "admin"
+                });
+            await _roleManager.CreateAsync(new IdentityRole
+            {
+                Name = "client"
+            });
+
+            await _userManager.CreateAsync(new UserEntity
+            {
+                FirstName = "Vitaly",
+                LastName = "Pupkin",
+                Email = "admin@test.com",
+                UserName = "Admin",
+                EmailConfirmed = true,
+                IsRemoved = false
+            }, "securePassword");
+            var book = _appContext.PrintingEditions.SingleOrDefault(pe => pe.Title == "Test PE");
+            var author = book.Authors.SingleOrDefault(a => a.Name == "Vasily");
+            if(author==null)
+            {
+                book.Authors.Add(_appContext.Authors.Single(a => a.Name == "Vasily"));
+            }
+            _appContext.SaveChanges();
         }
     }
 }
